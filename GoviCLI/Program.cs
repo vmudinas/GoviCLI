2054c6ef00a03289a8c91efb33e618327a0e71ca
Vitas <gliukaz@gmail.com>
2020-07-06 22:31:34 -0700

    feat: Initial project

---
+++ GoviCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoviCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
